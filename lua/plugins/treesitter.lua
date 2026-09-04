23c749e9426c19de1cbc4acc3990e6a46fa1dffd
Max de Hoyos <max.dehoyos@gmail.com>
2026-09-04 06:50:34 -0700

    rm ts-context
    terrible experience, always errors, not useful at all

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -6,110 +6,11 @@ return {
     build = ":TSUpdate",
   },
   {
-    "nvim-treesitter/nvim-treesitter-context",
-    opts = {
-      multiline_threshold = 1,
-    },
-  },
-  {
-    "nvim-treesitter/nvim-treesitter-textobjects",
-    config = function()
-      require("nvim-treesitter.configs").setup({
-        highlight = {
-          enable = true,
-        },
-        indent = {
-          enable = true,
-        },
-        endwise = {
-          enable = true,
-        },
-        autopairs = {
-          enable = true,
-        },
-        context_commentstring = {
-          enable = true,
-          enable_autocmd = false,
-        },
-        incremental_selection = {
-          enable = true,
-          keymaps = {
-            init_selection = "<C-space>", -- maps in normal mode to init the node/scope selection with ctrl+space
-            node_incremental = "<C-space>", -- increment to the upper named parent
-            node_decremental = "<bs>", -- decrement to the previous node
-            scope_incremental = "<noop>", -- increment to the upper scope (as defined in locals.scm)
-          },
-        },
-        auto_install = false,
-        textobjects = {
-          enable = true,
-          lookahead = true,
-          swap = {
-            enable = true,
-            swap_next = {
-              ["<leader>a"] = "@parameter.inner",
-            },
-            swap_previous = {
-              ["<leader>A"] = "@parameter.inner",
-            },
-          },
-          move = {
-            enable = true,
-            set_jumps = true,
-            goto_next_start = {
-              ["]f"] = "@function.outer",
-              ["]c"] = "@class.outer",
-              ["]a"] = "@parameter.outer",
-            },
-            goto_next_end = {
-              ["]F"] = "@function.outer",
-              ["]C"] = "@class.outer",
-              ["]A"] = "@parameter.outer",
-            },
-            goto_previous_start = {
-              ["[f"] = "@function.outer",
-              ["[c"] = "@class.outer",
-              ["[a"] = "@parameter.outer",
-            },
-            goto_previous_end = {
-              ["[F"] = "@function.outer",
-              ["[C"] = "@class.outer",
-              ["[A"] = "@parameter.outer",
-            },
-          },
-          select = {
-            enable = true,
-            keymaps = {
-              ["af"] = "@function.outer",
-              ["if"] = "@function.inner",
-
-              ["ac"] = "@conditional.outer",
-              ["ic"] = "@conditional.inner",
-
-              ["aa"] = "@parameter.outer",
-              ["ia"] = "@parameter.inner",
-
-              ["av"] = "@variable.outer",
-              ["iv"] = "@variable.inner",
-            },
-          },
-        },
-      })
-    end,
-  },
-  {
     "Wansmer/treesj",
     keys = { "<space>m", "<space>j", "<space>s" },
     dependencies = { "nvim-treesitter/nvim-treesitter" }, -- if you install parsers with `nvim-treesitter`
     config = function()
       require("treesj").setup({--[[ your config ]]
-      })
-      vim.keymap.set("n", "<leader>gS", function()
-        require("treesj").toggle()
-      end, {
-        noremap = true,
-        silent = true,
-        desc = "Toggle [s]plit/join",
       })
     end,
   },
