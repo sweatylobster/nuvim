a39ecc21b02e63a49e50a6074ff6e01101fd9fca
Max de Hoyos <max.dehoyos@gmail.com>
2026-09-04 06:51:08 -0700

    use oil float as default keybind, add preview

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -2,18 +2,19 @@ return {
   "stevearc/oil.nvim",
   ---@module 'oil'
   ---@type oil.SetupOpts
-  opts = {},
+  opts = {
+    float = { preview_split = "right" },
+  },
   -- Optional dependencies
   -- dependencies = { { "nvim-mini/mini.icons", opts = {} } },
   dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if you prefer nvim-web-devicons
   -- Lazy loading is not recommended because it is very tricky to make it work correctly in all situations.
   lazy = false,
   keys = {
-    { "-", ":Oil<CR>" },
     {
-      "<leader>-",
+      "-",
       function()
-        require("oil").toggle_float()
+        require("oil").toggle_float(nil, { preview = { horizontal = true } }, nil)
       end,
     },
   },
